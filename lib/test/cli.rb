5f26becba2fc2a5bc2a55b353ef84a2b867e9b5b
Carla <carla.holcomb25@gmail.com>
2019-11-27 12:38:19 -0500

    Update cli.rb

--- lib/test/cli.rb
+++ lib/test/cli.rb
@@ -1,30 +1,28 @@
 require "test/cli/version"
 
-class WorldsBestRestaurants::CLI
+class Test::CLI
 
   def call
-    WorldsBestRestaurants::Scraper.new.make_restaurants
-    puts "Welcome to the 50 Best Restaurants in the World"
+    Test::Scraper.new.create_actor
+    puts "Welcome to the Oscars Awards"
     start
   end
 
   def start
     puts ""
-    puts "What number restaurants would you like to see? 1-10, 11-20, 21-30, 31-40 or 41-50?"
+    print_actor_list(actor)
+    
+    puts ""
+    
+    puts "Which actor would you like to view the year, character and movie that they won an Oscar for?"
     input = gets.strip.to_i
+    
+    actor = Test::Actor.find(input)
 
-    print_restaurants(input)
-
-    puts ""
-    puts "What restaurant would you like more information on?"
-    input = gets.strip
-
-    restaurant = WorldsBestRestaurants::Restaurant.find(input.to_i)
-
-    print_restaurant(restaurant)
+    print_actor_info(actor)
 
     puts ""
-    puts "Would you like to see another restaurant? Enter Y or N"
+    puts "Would you like to see another actor's information? Enter Y or N"
 
     input = gets.strip.downcase
     if input == "y"
@@ -57,12 +55,11 @@ class WorldsBestRestaurants::CLI
     puts ""
   end
 
-  def print_restaurants(from_number)
+  def print_actor_list(from_number)
     puts ""
     puts "---------- Restaurants #{from_number} - #{from_number+9} ----------"
     puts ""
-    WorldsBestRestaurants::Restaurant.all[from_number-1, 10].each.with_index(from_number) do |restaurant, index|
-      puts "#{index}. #{restaurant.name} - #{restaurant.location}"
+    WorldsBestRestaurants::Restaurant.all"
     end
   end
 
