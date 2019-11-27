79fdf284ceca7328c5a44a261f3c19933c9429c9
Carla <carla.holcomb25@gmail.com>
2019-11-27 12:43:29 -0500

    Update actor.rb

--- lib/test/actor.rb
+++ lib/test/actor.rb
@@ -1,1 +1,44 @@
+class WorldsBestRestaurants::Restaurant
 
+  attr_accessor :name, :position, :location, :url, :head_chef, :website_url, :food_style, :best_dish, :contact, :description
+
+  @@all = []
+
+  def self.new_from_index_page(r)
+    self.new(
+      r.css("table tr td").text,
+      r.css("td").text,
+      r.css("td").text
+      )
+  end
+
+  def initialize(name=nil, url=nil, location=nil, position=nil)
+    @name = name
+    @url = url
+    @location = location
+    @position = position
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find(id)
+    self.all[id-1]
+  end
+
+  #def best_dish
+    #@best_dish ||= doc.css("div.c-4.nr.nt ul:nth-child(8) li").text
+    # @best_dish ||= doc.xpath("//div[@class='c-4 nr nt']/ul[3]/li").text
+  #end
+
+  #def food_style
+    #@food_style ||= doc.css("div.c-4.nr.nt ul:nth-child(6) li").text
+    # @food_style ||= doc.xpath("//div[@class='c-4 nr nt']/ul[2]/li").text
+  #end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
+  end
+end
