6e0a42d6946355e756cfaad73f35bcb4d90578b9
Carla <carla.holcomb25@gmail.com>
2020-01-05 14:26:12 -0500

    Update scraper.rb

--- lib/test/scraper.rb
+++ lib/test/scraper.rb
@@ -4,13 +4,20 @@ class Test::Scraper
     Nokogiri::HTML(open("https://www.filmsite.org/bestactor2.html"))
   end
 
-  def scrape_actor
-     self.get_page.css("div#t1-50 li")
+  def self.create_actor
+    
+     self.get_page.css("div#t1-50 li").each do |actor|
+       
+       
+       
+      Test::Actor.new(r)
+       
+     end
   end
 
-  def create_actor
-    scrape_actor.each do |r|
-      Test::Actor.new(r)
-    end
-  end
+  #def create_actor
+    #scrape_actor.each do |r|
+      #Test::Actor.new(r)
+    #end
+  #end
 end
