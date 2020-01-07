dbd59e9c00a82cdb5d0d57c9d5d81b9ee7b97149
Carla <carla.holcomb25@gmail.com>
2020-01-06 21:20:17 -0500

    Update scraper.rb

--- lib/test/scraper.rb
+++ lib/test/scraper.rb
@@ -8,10 +8,10 @@ class Test::Scraper
     
      new_page = Nokogiri::HTML(open("https://www.filmsite.org/bestactor2.html"))
     
-     new_page.css("div#mainBodyWrapper").each do |actor|
-       year = actor.css("table tr td:nth-child(1)").text.strip
-       name = actor.css("table tr td:nth-child(2)").text.strip
-       role = actor.css("table tr td:nth-child(3)").text.strip
+     new_page.css("div#mainBodyWrapper table tr").each do |actor|
+       year = actor.css("td:nth-child(1)").text.strip
+       name = actor.css("td:nth-child(2)").text.strip
+       role = actor.css("td:nth-child(3)").text.strip
        Test::Actor.new(year, name, role)
        end
   end
